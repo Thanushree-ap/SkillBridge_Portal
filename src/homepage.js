b1c3b3fe59323f6f72597c858581b9cc2adf3648
Thanushree-ap <thanuprabhakar17@gmail.com>
2025-11-29 00:05:36 +0530

    Delete src directory

--- src/homepage.js
+++
@@ -1,8 +0,0 @@
-// Auto-slide every 3 seconds
-let techCarousel = document.querySelector("#techCarousel");
-
-let carousel = new bootstrap.Carousel(techCarousel, {
-    interval: 3000,
-    ride: 'carousel',
-    pause: false
-});
